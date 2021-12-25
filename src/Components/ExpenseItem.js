3686dd335763381e4f95eee60a095e7e4ea2bad6
Mihai <mihai@twm.ro>
2021-12-25 07:27:05 +0200

    first draft

--- src/Components/ExpenseItem.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ExpenseItem() {
-     return (
-          <div>
-               <h2> Expense Items!</h2>
-          </div>
-     );
-}
---
+++ src/Components/Expenses/Expenses.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import Card from "../UI/Card";
+import "./Expenses.css";
+import ExpensesChart from "./ExpensesChart";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+
+const Expenses = (props) => {
+     const [filterYear, setFilterYear] = useState("2020");
+     const filterChangeHandler = (selectedYear) => {
+          setFilterYear(selectedYear);
+     };
+
+     const filteredExpenses = props.items.filter((expense) => {
+          return expense.date.getFullYear().toString() === filterYear;
+     });
+
+     return (
+          <Card className="expenses">
+               <ExpensesFilter
+                    selected={filterYear}
+                    onChangeFilter={filterChangeHandler}
+               />
+
+               <ExpensesChart expenses={filteredExpenses} />
+
+               <ExpensesList items={filteredExpenses} />
+          </Card>
+     );
+};
+
+export default Expenses;
